49e87c33de4b0877dc153bbf2e3e75ec2f6101e4
gcamon <gcamon>
2016-08-17 14:32:18 +0100

    fix a bug

--- noteApp.js
+++ noteApp.js
@@ -10,7 +10,7 @@ class Note {
 class NoteApplication {
 	//Initialize an instance of an Object
 	constructor(author) {
-		this.allNotes = []
+		this.allNotes = [];
 		this.author = author;
 		this.note_id = 0;	
 	}
@@ -20,11 +20,11 @@ class NoteApplication {
 		//Will create an object with property of the note content.
 	 	if(typeof note_content === 'object') {
 			var note = new Note(note_content);
-			note.properties._id += 1
+			note.properties._id += 1;
 			this.allNotes.push(note);			
-			console.log('note created successfully, note id is: ' + note.properties._id)
+			console.log('note created successfully, note id is: ' + note.properties._id);
 		} else {
-			console.log('Error! Type of ' + note_content + ' not supported')
+			console.log('Error! Type of ' + note_content + ' not supported');
 		}
 
 	}
@@ -33,10 +33,10 @@ class NoteApplication {
 		//Will list all the notes save to array.
 		// and display accordingly.
 		this.allNotes.forEach(function (item) {
-			console.log(item)
+			console.log(item);
 			console.log('note id :' + item.properties._id);
-			console.log(item.properties.note)
-			console.log('Author by : ' + item.properties.author)
+			console.log(item.properties.note);
+			console.log('Author by : ' + item.properties.author);
 		})
 	}
 
@@ -45,14 +45,14 @@ class NoteApplication {
 		if(typeof note_id === 'number'){
 		this.allNotes.forEach(function (item) {
 			if(note_id === item.properties._id) {
-				console.log(item.properties.note)
+				console.log(item.properties.note);
 			} else {
-				console.log('no such content')
+				console.log('no such content');
 			}
 		})
 
 		} else {
-			console.log('Invalid note id! Please enter a valid Number')
+			console.log('Invalid note id! Please enter a valid Number');
 		}
 
 	}
@@ -65,13 +65,13 @@ class NoteApplication {
 			if(item.properties.note.match(reg)){
 				console.log('Note Id : ' + item.properties._id);
 				console.log(item.properties.note);
-				console.log('Author by : ' + item.properties.author)
+				console.log('Author by : ' + item.properties.author);
 			} else {
-				console.log('No such content availabe')
+				console.log('No such content availabe');
 			}		
 		})
 		} else {
-			console.log('Invalid search input')
+			console.log('Invalid search input');
 		}
 
 	}
@@ -82,13 +82,13 @@ class NoteApplication {
 		this.allNotes.forEach(function (item) {
 			if(item.properties._id === note_id){
 				item.properties.note = " ";
-				console.log('note deleted successfully!')
+				console.log('note deleted successfully!');
 			} else {
-				console.log('No such note content available')
+				console.log('No such note content available');
 			}
 		})
 		} else {
-			console.log('invalid input. Must be an integer')
+			console.log('invalid input. Must be an integer');
 		}
 
 	}
@@ -99,13 +99,13 @@ class NoteApplication {
 		this.allNotes.forEach(function (item) {
 			if (item.properties._id === note_id) {
 				item.properties.note = new_content;
-				console.log('content edited successfully!')
+				console.log('content edited successfully!');
 			} else {
-				console.log('Invalid note ID')
+				console.log('Invalid note ID');
 			}
 		})
 		} else {
-			console.log('invalid input format. Id Must be an integer and content must be string!')
+			console.log('invalid input format. Id Must be an integer and content must be string!');
 		}
 	}
 }
@@ -114,10 +114,19 @@ var note = {
 	author: "Obinna",
 	_id : 0,
 	note : "i have a dream"
-}
+};
 
 var obinna = new NoteApplication("obinna");
 obinna.create(note);
 obinna.listNotes();
 obinna.get(1);
 obinna.edit(1, 'i have a dream');
+
+var tony = {
+	author: "uche",
+	_id : 0,
+	note : "He saw a Ghost"
+}
+
+var uche = new NoteApplication('uche')
+uche.create(tony)
